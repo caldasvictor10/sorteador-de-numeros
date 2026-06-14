9a0b3acc4950f5162effe0a973f0e9f6faa3740f
caldasvictor10 <caldasvictor10@gmail.com>
2026-06-14 10:59:52 -0300

    Atualização 02 do sorteador de números.

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-function sortear () {
+function sortear (){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
     
-    let numero = obterNumeroAleatorio (de, ate);
-    alert (numero);
+    let sorteados = [];
+    let numero;
 
+    for (let i = 0; i < quantidade; i++) {
+      numero = obterNumeroAleatorio(de, ate);
+      sorteados.push(numero);
+    }
+
+    alert(sorteados);
 }
 
 function obterNumeroAleatorio (min, max) {
